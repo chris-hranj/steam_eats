748e92646d0f7663c5e3dee8203f6ce8c256303c
chris-hranj <sprtn034@gmail.com>
2015-01-30 13:46:06 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var requestify = require('requestify'); 
+
+var steamid;
+requestify.get('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=58B5286A7176E71A467B315C0F564330&vanityurl=zuuku').then(function(response) {
+// Get the response body (JSON parsed - JSON response or jQuery object in case of XML response)
+steamid = response.getBody().response.steamid;
+});
+
+/* GET home page. */
+router.get('/', function(req, res) {
+	
+
+	res.render('index', { title: 'Express', steam: steamid });
+});
+
+// router.get('/users/:id', function(req, res) {
+// 	// res.render('index', {
+// 	// 	title:'ass',
+// 	// 	age: 33
+// 	// });
+// console.log(req.params);
+// res.send(200);
+// });
+
+module.exports = router;
